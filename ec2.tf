1a050bf919b432c7a885d27338f545824f1b1431
Dinh Le Nguyen <dnhlenguyen@gmail.com>
2025-04-28 21:41:53 +0200

    Add ec2.tf for EC2 instance creation

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+########################################
+# EC2 Instances
+########################################
+
+# EC2 Instance A (Private Subnet A)
+resource "aws_instance" "web_a_internship_dinh" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.private_subnet_a.id
+  vpc_security_group_ids      = [aws_security_group.web_internship_dinh.id]
+  associate_public_ip_address = false  # Instance is private (NAT outbound)
+
+  tags = {
+    Name = "ec2_web_a_internship_dinh"
+  }
+
+  user_data = var.user_data_script_a
+}
+
+# EC2 Instance B (Private Subnet B)
+resource "aws_instance" "web_b_internship_dinh" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.private_subnet_b.id
+  vpc_security_group_ids      = [aws_security_group.web_internship_dinh.id]
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "ec2_web_b_internship_dinh"
+  }
+
+  user_data = var.user_data_script_b
+}
