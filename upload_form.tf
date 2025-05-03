6a6801ad590123f1ff25a6a33677759ad6885188
Dinh Le Nguyen <dnhlenguyen@gmail.com>
2025-05-03 23:44:40 +0200

    Adjust formatting

--- upload_form.tf
+++ upload_form.tf
@@ -34,7 +34,7 @@ resource "aws_s3_bucket_cors_configuration" "upload_cors" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["PUT", "POST", "GET"]
-    allowed_origins = ["*"] 
+    allowed_origins = ["*"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
