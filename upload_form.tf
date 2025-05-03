5a264c06f7be3d6bed0977bde4ada0aada38be69
Dinh Le Nguyen <dnhlenguyen@gmail.com>
2025-05-03 18:48:28 +0200

    ADd CORS configuration to S3 bucket

--- upload_form.tf
+++ upload_form.tf
@@ -25,6 +25,20 @@ resource "aws_s3_bucket_public_access_block" "uploads_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+########################################
+# CORS configuration for browser upload
+########################################
+resource "aws_s3_bucket_cors_configuration" "upload_cors" {
+  bucket = aws_s3_bucket.upload_form.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET"]
+    allowed_origins = ["*"] 
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
 
 ########################################
 # Cognito Identity Pool (for unauthenticated users)
