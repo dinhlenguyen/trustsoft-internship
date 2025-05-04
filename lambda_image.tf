8d9c83e05a90a2ef35e8e2cf0f1bb19252430ccf
Dinh Le Nguyen <dnhlenguyen@gmail.com>
2025-05-04 14:02:26 +0200

    Update S3 bucket policy configurations

--- lambda_image.tf
+++ lambda_image.tf
@@ -21,6 +21,10 @@ resource "aws_s3_bucket_public_access_block" "transformed_block" {
 resource "aws_s3_bucket_policy" "allow_public_read" {
   bucket = aws_s3_bucket.transformed_images.id
 
+  depends_on = [
+    aws_s3_bucket_public_access_block.transformed_block
+  ]
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
