4ac38f15fbcd1cdeab3f19911d8d4533199b0ba1
Dinh Le Nguyen <dnhlenguyen@gmail.com>
2025-05-05 11:53:11 +0200

    Add cwagent and ssm association

--- iam.tf
+++ iam.tf
@@ -60,3 +60,85 @@ resource "aws_iam_instance_profile" "ssm_profile_internship_dinh" {
   name = "ssm-s3-profile-internship-dinh"
   role = aws_iam_role.ssm_s3_internship_dinh.name
 }
+
+########################################
+# SSM Document: CloudWatch Agent Config
+########################################
+
+data "aws_ssm_document" "cwagent_document" {
+  name = "AmazonCloudWatch-ManageAgent"
+}
+
+########################################
+# SSM Parameter
+########################################
+resource "aws_ssm_parameter" "cwagent_config" {
+  name        = "AmazonCloudWatch-linux"
+  type        = "String"
+  description = "CloudWatch Agent configuration for EC2 instances"
+  overwrite   = true
+  tier        = "Standard"
+  value       = file("${path.module}/cwagent-config.json")
+
+  tags = {
+    Name = "cwagent-config"
+  }
+}
+
+########################################
+# SSM Association for EC2 Instance A
+########################################
+
+resource "aws_ssm_association" "cwagent_association_a" {
+  name             = data.aws_ssm_document.cwagent_document.name
+  association_name = "cwagent-ec2b-association"
+
+  targets {
+    key    = "InstanceIds"
+    values = [aws_instance.web_a_internship_dinh.id]
+  }
+
+  parameters = {
+    action                        = "configure"
+    mode                          = "ec2"
+    optionalConfigurationSource   = "ssm"
+    optionalConfigurationLocation = "AmazonCloudWatch-linux"
+    optionalRestart               = "yes"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cloudwatch_agent_attach,
+    aws_instance.web_a_internship_dinh,
+    aws_ssm_parameter.cwagent_config
+  ]
+}
+
+
+########################################
+# SSM Association for EC2 Instance B
+########################################
+
+resource "aws_ssm_association" "cwagent_association_b" {
+  name             = data.aws_ssm_document.cwagent_document.name
+  association_name = "cwagent-ec2b-association"
+
+  targets {
+    key    = "InstanceIds"
+    values = [aws_instance.web_b_internship_dinh.id]
+  }
+
+  parameters = {
+    action                        = "configure"
+    mode                          = "ec2"
+    optionalConfigurationSource   = "ssm"
+    optionalConfigurationLocation = "AmazonCloudWatch-linux"
+    optionalRestart               = "yes"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cloudwatch_agent_attach,
+    aws_instance.web_b_internship_dinh,
+    aws_ssm_parameter.cwagent_config
+  ]
+}
+
