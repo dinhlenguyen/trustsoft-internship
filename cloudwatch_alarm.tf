869685d1a3554e9c60cfb90ac6c33ff3d687feab
Dinh Le Nguyen <dnhlenguyen@gmail.com>
2025-05-05 09:53:47 +0200

    Add memory and disc usage metrics

--- cloudwatch_alarm.tf
+++ cloudwatch_alarm.tf
@@ -14,7 +14,7 @@ resource "aws_sns_topic_subscription" "email_internship_dinh" {
 }
 
 ########################################
-# CPU Utilization Alarms with SNS Actions
+# CPU Utilization Alarms
 ########################################
 
 resource "aws_cloudwatch_metric_alarm" "ec2_a_cpu_high_internship_dinh" {
@@ -25,18 +25,14 @@ resource "aws_cloudwatch_metric_alarm" "ec2_a_cpu_high_internship_dinh" {
   namespace           = "AWS/EC2"
   period              = 120
   statistic           = "Average"
-  threshold           = 10
-
-  alarm_description = "Alarm when EC2-A CPU exceeds 10% for 4 minutes"
+  threshold           = 90
   dimensions = {
     InstanceId = aws_instance.web_a_internship_dinh.id
   }
-
   treat_missing_data = "notBreaching"
-
-  # send to SNS topic when alarm state changes
-  alarm_actions = [aws_sns_topic.alarms_internship_dinh.arn]
-  ok_actions    = [aws_sns_topic.alarms_internship_dinh.arn]
+  alarm_description  = "Alarm when EC2-A CPU exceeds 90% for 4 minutes"
+  alarm_actions      = [aws_sns_topic.alarms_internship_dinh.arn]
+  ok_actions         = [aws_sns_topic.alarms_internship_dinh.arn]
 
   tags = {
     Name = "alarm_ec2_a_cpu_internship_dinh"
@@ -51,19 +47,96 @@ resource "aws_cloudwatch_metric_alarm" "ec2_b_cpu_high_internship_dinh" {
   namespace           = "AWS/EC2"
   period              = 120
   statistic           = "Average"
-  threshold           = 10
-
-  alarm_description = "Alarm when EC2-B CPU exceeds 10% for 4 minutes"
+  threshold           = 90
   dimensions = {
     InstanceId = aws_instance.web_b_internship_dinh.id
   }
-
   treat_missing_data = "notBreaching"
-
-  alarm_actions = [aws_sns_topic.alarms_internship_dinh.arn]
-  ok_actions    = [aws_sns_topic.alarms_internship_dinh.arn]
+  alarm_description  = "Alarm when EC2-B CPU exceeds 90% for 4 minutes"
+  alarm_actions      = [aws_sns_topic.alarms_internship_dinh.arn]
+  ok_actions         = [aws_sns_topic.alarms_internship_dinh.arn]
 
   tags = {
     Name = "alarm_ec2_b_cpu_internship_dinh"
   }
+}
+
+########################################
+# Memory Usage Alarms (Custom Metrics)
+########################################
+
+resource "aws_cloudwatch_metric_alarm" "ec2_a_memory_high" {
+  alarm_name          = "ec2-a-memory-high-internship-dinh"
+  namespace           = "CWAgent"
+  metric_name         = "mem_used_percent"
+  dimensions          = { InstanceId = aws_instance.web_a_internship_dinh.id }
+  statistic           = "Average"
+  period              = 120
+  evaluation_periods  = 2
+  threshold           = 90
+  comparison_operator = "GreaterThanThreshold"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Memory usage > 90% on EC2 A"
+  alarm_actions       = [aws_sns_topic.alarms_internship_dinh.arn]
+  ok_actions          = [aws_sns_topic.alarms_internship_dinh.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "ec2_b_memory_high" {
+  alarm_name          = "ec2-b-memory-high-internship-dinh"
+  namespace           = "CWAgent"
+  metric_name         = "mem_used_percent"
+  dimensions          = { InstanceId = aws_instance.web_b_internship_dinh.id }
+  statistic           = "Average"
+  period              = 120
+  evaluation_periods  = 2
+  threshold           = 90
+  comparison_operator = "GreaterThanThreshold"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Memory usage > 90% on EC2 B"
+  alarm_actions       = [aws_sns_topic.alarms_internship_dinh.arn]
+  ok_actions          = [aws_sns_topic.alarms_internship_dinh.arn]
+}
+
+########################################
+# Disk Usage Alarms (Custom Metrics)
+########################################
+
+resource "aws_cloudwatch_metric_alarm" "ec2_a_disk_high" {
+  alarm_name          = "ec2-a-disk-high-internship-dinh"
+  namespace           = "CWAgent"
+  metric_name         = "disk_used_percent"
+  dimensions          = {
+    InstanceId = aws_instance.web_a_internship_dinh.id,
+    path       = "/",
+    fstype     = "xfs"
+  }
+  statistic           = "Average"
+  period              = 120
+  evaluation_periods  = 2
+  threshold           = 90
+  comparison_operator = "GreaterThanThreshold"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Disk usage > 90% on EC2 A"
+  alarm_actions       = [aws_sns_topic.alarms_internship_dinh.arn]
+  ok_actions          = [aws_sns_topic.alarms_internship_dinh.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "ec2_b_disk_high" {
+  alarm_name          = "ec2-b-disk-high-internship-dinh"
+  namespace           = "CWAgent"
+  metric_name         = "disk_used_percent"
+  dimensions          = {
+    InstanceId = aws_instance.web_b_internship_dinh.id,
+    path       = "/",
+    fstype     = "xfs"
+  }
+  statistic           = "Average"
+  period              = 120
+  evaluation_periods  = 2
+  threshold           = 90
+  comparison_operator = "GreaterThanThreshold"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Disk usage > 90% on EC2 B"
+  alarm_actions       = [aws_sns_topic.alarms_internship_dinh.arn]
+  ok_actions          = [aws_sns_topic.alarms_internship_dinh.arn]
 }
