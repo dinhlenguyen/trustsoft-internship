8f9771517c8e24b61878c73dfefbc2cb7b58fabc
Dinh Le Nguyen <dnhlenguyen@gmail.com>
2025-05-07 13:44:46 +0200

    Remove unused feature and assets

--- outputs.tf
+++ outputs.tf
@@ -2,13 +2,3 @@ output "alb_dns_name" {
   description = "DNS name of the ALB"
   value       = aws_lb.alb_internship_dinh.dns_name
 }
-
-output "identity_pool_id" {
-  description = "Cognito identity ID"
-  value       = aws_cognito_identity_pool.cognito_internship_dinh.id
-}
-
-output "rds_endpoint" {
-  description = "RDS MySQL endpoint"
-  value       = aws_db_instance.mysql.endpoint
-}
